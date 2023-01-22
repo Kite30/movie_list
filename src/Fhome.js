c895c7269f3fe6d8f8ae65dfc252c761a7b25ef4
KSHITIJ SRIVASTAVA <kshitij05.ks@gmail.com>
2023-01-22 18:05:32 +0530

    Movie List

---
+++ src/Fhome.js
@@ -0,0 +1,48 @@
+import {Component} from 'react';
+import React, { useEffect, useState } from "react";
+import {useParams} from "react-router-dom";
+import './home.css';
+
+const Fhome = (props) =>{
+
+    const [user, setUser] = useState({image:null,rating:null});
+    const params = useParams();
+    let url="https://api.tvmaze.com/shows/";
+  let search_term =params.id;
+  console.log(search_term);
+  let fx = url.concat(search_term);
+    
+    const fetchData = () => {
+        return fetch(fx)
+              .then((response) => response.json())
+              .then((data) => setUser(data));
+      }
+    
+      useEffect(() => {
+        fetchData();
+      },[])
+    return(
+        <div class="fd">
+            <div class="pk">
+
+                {user.image?<img src={user.image.medium} alt="unable to load"/>:""};
+
+            </div>
+            
+            <div class="gh">
+
+                <h3>{user.name}</h3>
+                {user.rating?<p><b> Rating: {user.rating.average} </b> </p>:""}
+                <p>Release Date: {user.premiered} | Length: {user.runtime} </p>
+                <p>Movie Description</p>
+                <p>{user.summary}</p>
+
+            </div>
+        </div>
+        
+      
+    );
+
+}
+
+export default Fhome
